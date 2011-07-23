bb2a881ea036596d6587a80816bfe2daa791d5ab
Ofer Fridman <oferfrid@gmail.com>
2011-07-23 14:51:18 +0300

    Differentiate tube from strains
    Signed-off-by: Ofer Fridman <oferfrid@gmail.com>

---
+++ StrainParameters.cs
@@ -0,0 +1,54 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: oferfrid
+ * Date: 23/07/2011
+ * Time: 14:01
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace IritSimulation
+{
+	/// <summary>
+	/// Description of StrainParameters.
+	/// </summary>
+	public struct StrainParameters
+	{
+		public string Name;
+		
+		public double No;
+		public double PersistersLevel;
+		
+		//lag parameters
+		public double LagMeanNormal;
+		public double LagMeanPersisters;
+		
+		//division parameters
+		public double DivMean;
+		public double DivVar;
+		
+		public Utils.LognormalParameters DivLognormalParameters ;
+		
+		public StrainParameters(
+			string Name,
+			double No,
+			double PersistersLevel,
+			double LagMeanNormal,
+			double LagMeanPersisters,
+			double DivMean,
+			double DivVar
+		)
+		{
+			this.Name = Name;
+			this.No = No  ;
+			this.PersistersLevel =  PersistersLevel ;
+			this.LagMeanNormal =  LagMeanNormal ;
+			this.LagMeanPersisters =  LagMeanPersisters ;
+			this.DivMean = DivMean  ;
+			this.DivVar = DivVar  ;
+			DivLognormalParameters = Utils.CommuteLognormalParameters(DivMean,DivVar);
+		}
+
+	}
+}
---
+++ TubeParameters.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: owner
+ * Date: 22/07/2011
+ * Time: 13:52
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace IritSimulation
+{
+	/// <summary>
+	/// Description of TubeParameters.
+	/// </summary>
+	public struct TubeParameters //: IEquatable<TubeParameters>
+	{
+		public double Nmax;
+		public int NumberOfStrains;
+		public StrainParameters[] Strains;
+		
+		public TubeParameters(
+			double Nmax,
+			StrainParameters[] Strains
+		)
+			
+		{
+			this.Nmax = Nmax;
+			this.Strains = Strains;
+			NumberOfStrains = Strains.Length;
+		}
+	}
+}
